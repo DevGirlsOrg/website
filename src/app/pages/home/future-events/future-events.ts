fd74c027e13c96a0f447bc279de196ba04d4a9d3
arolleaguekeng <arolleaguekeng@gmail.com>
2026-01-12 22:15:13 +0100

    create components structure

---
+++ src/app/pages/home/future-events/future-events.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-future-events',
+  imports: [],
+  templateUrl: './future-events.html',
+  styleUrl: './future-events.css',
+})
+export class FutureEvents {
+
+}
